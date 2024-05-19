93aa5881102f2cf25ee858385975c47ba5e54c4d
NabilaFerdousPrapty <128033656+NabilaFerdousPrapty@users.noreply.github.com>
2024-05-19 23:51:28 +0600

    functionality started

--- src/components/Home/Rooms.jsx
+++ src/components/Home/Rooms.jsx
@@ -3,22 +3,39 @@ import Card from './Card'
 import Container from '../Shared/Container'
 import Heading from '../Shared/Heading'
 import LoadingSpinner from '../Shared/LoadingSpinner'
+import { useQuery } from '@tanstack/react-query'
+import useAxiosSecure from '../../hooks/useAxiosSecure'
+
 
 const Rooms = () => {
-  const [rooms, setRooms] = useState([])
-  const [loading, setLoading] = useState(false)
 
-  useEffect(() => {
-    setLoading(true)
-    fetch(`./rooms.json`)
-      .then(res => res.json())
-      .then(data => {
-        setRooms(data)
-        setLoading(false)
-      })
-  }, [])
+  const axiosSecure=useAxiosSecure();
 
-  if (loading) return <LoadingSpinner />
+   const {data:rooms=[],isLoading}=useQuery({
+    queryKey:['rooms'],
+    queryFn:async()=>{
+      const {data}=await axiosSecure.get('/rooms')
+      // console.log(data);
+      return data;
+    }
+   }
+
+   );
+   
+ 
+  //  console.log(query);
+  //  console.log(query.data);
+  // useEffect(() => {
+  //   setLoading(true)
+  //   fetch(`http://localhost:5000/rooms`)
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setRooms(data)
+  //       setLoading(false)
+  //     })
+  // }, [])
+
+  if (isLoading) return <LoadingSpinner />
 
   return (
     <Container>
