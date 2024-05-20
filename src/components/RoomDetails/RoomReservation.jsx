0bd6724d4134099649bae0cb25f2c943341abca9
NabilaFerdousPrapty <128033656+NabilaFerdousPrapty@users.noreply.github.com>
2024-05-20 14:46:43 +0600

    tanstack query implemented

--- src/components/RoomDetails/RoomReservation.jsx
+++ src/components/RoomDetails/RoomReservation.jsx
@@ -1,11 +1,32 @@
 import PropTypes from 'prop-types'
 import Button from '../Shared/Button/Button'
+// import useAxiosSecure from '../../hooks/useAxiosSecure';
+// import { useQuery } from '@tanstack/react-query';
+// import { useParams } from 'react-router-dom';
+// import LoadingSpinner from '../Shared/LoadingSpinner';
 
-const RoomReservation = ({ room }) => {
+const RoomReservation = ({room}) => {
+  // const { id } = useParams();
+  // console.log('Room ID:', id);
+  // const axiosSecure=useAxiosSecure();
+
+  //  const {data:room={},isLoading}=useQuery({
+  //   queryKey:['room',id],
+  //   queryFn:async()=>{
+  //     const {data}=await axiosSecure.get(`/room/${id}`)
+  //     // console.log(data);
+  //     return data;
+  //   }
+  //  }
+
+  //  );
+  //  if (isLoading) return <LoadingSpinner />
+  //  console.log(room);
   return (
     <div className='rounded-xl border-[1px] border-neutral-200 overflow-hidden bg-white'>
       <div className='flex items-center gap-1 p-4'>
-        <div className='text-2xl font-semibold'>$ {room?.price}</div>
+        <div className='text-2xl font-semibold'>$ {room.price}</div>
+       
         <div className='font-light text-neutral-600'>night</div>
       </div>
       <hr />
