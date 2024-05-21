47f3ab5c6bb1f7a8f4853b6619f58500f9ab3af8
NabilaFerdousPrapty <128033656+NabilaFerdousPrapty@users.noreply.github.com>
2024-05-21 18:37:41 +0600

    day 1 completed

--- src/components/RoomDetails/RoomReservation.jsx
+++ src/components/RoomDetails/RoomReservation.jsx
@@ -1,27 +1,21 @@
 import PropTypes from 'prop-types'
 import Button from '../Shared/Button/Button'
-// import useAxiosSecure from '../../hooks/useAxiosSecure';
+import { useState } from 'react';
+import { DateRange } from 'react-date-range';
+
 // import { useQuery } from '@tanstack/react-query';
 // import { useParams } from 'react-router-dom';
 // import LoadingSpinner from '../Shared/LoadingSpinner';
 
 const RoomReservation = ({room}) => {
-  // const { id } = useParams();
-  // console.log('Room ID:', id);
-  // const axiosSecure=useAxiosSecure();
-
-  //  const {data:room={},isLoading}=useQuery({
-  //   queryKey:['room',id],
-  //   queryFn:async()=>{
-  //     const {data}=await axiosSecure.get(`/room/${id}`)
-  //     // console.log(data);
-  //     return data;
-  //   }
-  //  }
-
-  //  );
-  //  if (isLoading) return <LoadingSpinner />
-  //  console.log(room);
+  const [state, setState] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: 'selection'
+    }
+  ]);
+  
   return (
     <div className='rounded-xl border-[1px] border-neutral-200 overflow-hidden bg-white'>
       <div className='flex items-center gap-1 p-4'>
@@ -30,7 +24,16 @@ const RoomReservation = ({room}) => {
         <div className='font-light text-neutral-600'>night</div>
       </div>
       <hr />
-      <div className='flex justify-center'>{/* Calender */}</div>
+      <div className='flex justify-center'>
+        <DateRange
+        showDateDisplay={false}
+        rangeColors={['#f43f5e']}
+        editableDateInputs={true}
+        onChange={item => setState([item.selection])}
+        moveRangeOnFirstSelection={false}
+        ranges={state}
+      />
+      </div>
       <hr />
       <div className='p-4'>
         <Button label={'Reserve'} />
